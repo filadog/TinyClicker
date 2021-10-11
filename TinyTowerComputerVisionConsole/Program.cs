37cd309fc4c0d6a9de8609b340b7fb149a76e358
filadog <filimonov1995@gmail.com>
2021-10-12 01:36:49 +0300

    Add project files.

---
+++ TinyTowerComputerVisionConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinyTowerComputerVisionConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
