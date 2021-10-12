2a4d189ac61d9f05ad9ef1e46dff667eee7b8cbb
filadog <filimonov1995@gmail.com>
2021-10-12 03:32:17 +0300

    Added OpenCvSharp package

--- TinyTowerComputerVisionConsole/Program.cs
+++ TinyTowerComputerVisionConsole/Program.cs
@@ -4,7 +4,7 @@ namespace TinyTowerComputerVisionConsole
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
         }
