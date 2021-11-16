5c552a5a9746c8e6ded3e52b0deeba5e778d96e8
filadog <filimonov1995@gmail.com>
2021-11-16 15:24:20 +0300

    Update Config.cs

--- TinyClicker/scripts/Config.cs
+++ TinyClicker/scripts/Config.cs
@@ -32,51 +32,6 @@ namespace TinyClickerUI
     {
         static readonly string configPath = Environment.CurrentDirectory + @"\Config.txt";
 
-        //public static void CreateNewConfigCommand()
-        //{
-        //    float elevatorSpeed;
-        //    bool vipPackage;
-        //    int floors;
-
-        //    try
-        //    {
-        //        Console.WriteLine("New config. Enter the value and press enter.\nDo you have the VIP package? (!) enter true or false");
-        //        bool.TryParse(Console.ReadLine(), out vipPackage);
-
-        //        Console.WriteLine("Provide value for the elevator speed (e.g: 10 or 9.25), leave empty for default value (10)");
-        //        string input = Console.ReadLine();
-        //        if (input.Length == 0)
-        //        {
-        //            elevatorSpeed = 10f;
-        //        }
-        //        else
-        //        {
-        //            elevatorSpeed = float.Parse(input);
-        //            if (elevatorSpeed > 10 || elevatorSpeed < 0)
-        //            {
-        //                Console.WriteLine("Error: elevator speed cannot be lower than zero or greater than 10");
-        //                CreateNewConfigCommand();
-        //            }
-        //        }
-
-        //        Console.WriteLine("Provide the current number of floors (e.g: 15)");
-        //        floors = Convert.ToInt32(Console.ReadLine());
-
-        //        Config config = new Config(vipPackage, elevatorSpeed, floors);
-
-        //        var options = new JsonSerializerOptions { WriteIndented = true };
-        //        string json = JsonSerializer.Serialize(config, options);
-        //        File.WriteAllText(configPath, json);
-        //        Console.WriteLine("Success! Restart the app");
-        //        Console.ReadKey();
-        //    }
-        //    catch (Exception)
-        //    {
-        //        Console.WriteLine("Error: Incorrect input, make sure you use the correct values");
-        //        CreateNewConfigCommand();
-        //    }
-        //}
-
         public static void AddNewFloor()
         {
             var config = Clicker.currentConfig;
