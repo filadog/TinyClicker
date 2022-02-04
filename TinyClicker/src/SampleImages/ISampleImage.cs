6fd09cdce51af66fd84ec1679ac01b22d2034a33
filadog <filimonov1995@gmail.com>
2022-02-04 04:01:52 +0300

    Small refactoring of the SampleImage class

---
+++ TinyClicker/src/SampleImages/ISampleImage.cs
@@ -0,0 +1,13 @@
+﻿using OpenCvSharp;
+using System;
+
+
+namespace TinyClicker.src.SampleImages
+{
+    internal interface ISampleImage
+    {
+        Mat Mat { get; }
+
+        Mat GenerateMat(string imagePath);
+    }
+}
