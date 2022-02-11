979f7f665fe1867cb535970d0b8742f829af2051
filadog <filimonov1995@gmail.com>
2022-02-11 18:04:37 +0300

    Configuration refactoring

---
+++ TinyClicker/src/Configuration/Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TinyClickerUI
+{
+    public class Config
+    {
+        private bool _vipPackage;
+        private float _elevatorSpeed;
+        private int _floorsNumber;
+        private int _coins;
+        private DateTime _lastRebuildTime;
+
+        public Config() : this(true, 10f, 3) { }
+        public Config(bool vip, float elevatorSpeed, int floorsNumber)
+        {
+            VipPackage = vip;
+            ElevatorSpeed = elevatorSpeed;
+            FloorsNumber = floorsNumber;
+            Coins = 0;
+        }
+
+        public bool VipPackage { get => _vipPackage; set => _vipPackage = value; }
+        public float ElevatorSpeed { get => _elevatorSpeed; set => _elevatorSpeed = value; }
+        public int FloorsNumber { get => _floorsNumber; set => _floorsNumber = value; }
+        public int Coins { get => _coins; set => _coins = value; }
+        public DateTime LastRebuildTime { get => _lastRebuildTime; set => _lastRebuildTime = value; }
+    }
+}
