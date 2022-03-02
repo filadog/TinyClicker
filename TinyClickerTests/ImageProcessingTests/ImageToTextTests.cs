75c1de4c6c5547e6f4bfa95f734c5b8f97b7abe0
filadog <filimonov1995@gmail.com>
2022-03-02 22:46:00 +0300

    More tests

---
+++ TinyClickerTests/ImageProcessingTests/ImageToTextTests.cs
@@ -0,0 +1,86 @@
+﻿using TinyClickerUI;
+using Xunit;
+
+namespace TinyClickerTests.ImageProcessing
+{
+    public class ImageToTextTests
+    {
+        // Tesseract Tests
+        // Accuracy of the OCR for the exact numbers
+
+        [Fact]
+        public void ParseBalanceFromImage_96245()
+        {
+            var image = TestHelper.LoadBalanceSample("96245");
+            int expected = 96245;
+            int actual = ImageToText.ParseBalance(image);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ParseBalanceFromImage_188214()
+        {
+            var image = TestHelper.LoadBalanceSample("188214");
+            int expected = 188214;
+            int actual = ImageToText.ParseBalance(image);
+            Assert.Equal(expected, actual);
+        }
+
+        // Accuracy of the OCR for the amount of digits
+
+        [Fact]
+        public void ParseBalanceFromImage_8_digits_1()
+        {
+            var image = TestHelper.LoadBalanceSample("10130000");
+            int expectedLength = 8;
+            int actual = ImageToText.ParseBalance(image).ToString().Length;
+            Assert.Equal(expectedLength, actual);
+        }
+
+        [Fact]
+        public void ParseBalanceFromImage_8_digits_2()
+        {
+            var image = TestHelper.LoadBalanceSample("11220000");
+            int expectedLength = 8;
+            int actualLength = ImageToText.ParseBalance(image).ToString().Length;
+            Assert.Equal(expectedLength, actualLength);
+        }
+
+        [Fact]
+        public void ParseBalanceFromImage_7_digits()
+        {
+            var image = TestHelper.LoadBalanceSample("1253000");
+            int expectedLength = 7;
+            int actualLength = ImageToText.ParseBalance(image).ToString().Length;
+            Assert.Equal(expectedLength, actualLength);
+        }
+
+        [Fact]
+        public void ParseBalanceFromImage_6_digits_1()
+        {
+            var image = TestHelper.LoadBalanceSample("109407");
+            int expectedLength = 6;
+            int actualLength = ImageToText.ParseBalance(image).ToString().Length;
+            Assert.Equal(expectedLength, actualLength);
+        }
+
+        [Fact]
+        public void ParseBalanceFromImage_6_digits_2()
+        {
+            var image = TestHelper.LoadBalanceSample("445007");
+            int expectedLength = 6;
+            int actualLength = ImageToText.ParseBalance(image).ToString().Length;
+            Assert.Equal(expectedLength, actualLength);
+        }
+
+
+        [Fact]
+        public void ParseBalanceFromImage_5_digits()
+        {
+            var image = TestHelper.LoadBalanceSample("64357");
+            int expectedLength = 5;
+            int actualLength = ImageToText.ParseBalance(image).ToString().Length;
+            Assert.Equal(expectedLength, actualLength);
+        }
+    }
+}
---
+++ TinyClickerTests/ImageProcessingTests/TestHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace TinyClickerTests
+{
+    internal static class TestHelper
+    {
+        public static Image LoadBalanceSample(string imageName)
+        {
+            string fileName = $@".\samples\Tests\BalanceImageSamples\{imageName}.png";
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException($"Could not find the image at: {fileName}");
+            }
+            return Image.FromFile(fileName);
+        }
+    }
+}
