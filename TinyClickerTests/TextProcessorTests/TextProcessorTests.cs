f812455c923bffd546bb1fcc90df62cb1da6050f
filadog <filimonov1995@gmail.com>
2022-03-02 04:10:40 +0300

    New tests for the balance parser

--- TinyClickerTests/TextProcessorTests/TextProcessorTests.cs
+++ TinyClickerTests/TextProcessorTests/TextProcessorTests.cs
@@ -38,5 +38,50 @@ namespace TinyClickerTests.TextProcessorTests
 
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void ParseBalanceFromImage_10130000()
+        {
+            string fileName = @".\samples\Tests\BalanceImageSamples\10130000.png";
+            if (!File.Exists(fileName))
+            {
+                throw new ArgumentException($"Could not find file at path: {fileName}");
+            }
+            var image = Image.FromFile(fileName);
+            int expectedLength = 8;
+            int actual = TextProcessor.ParseBalance(image).ToString().Length;
+
+            Assert.Equal(expectedLength, actual);
+        }
+
+        [Fact]
+        public void ParseBalanceFromImage_11220000()
+        {
+            string fileName = @".\samples\Tests\BalanceImageSamples\11220000.png";
+            if (!File.Exists(fileName))
+            {
+                throw new ArgumentException($"Could not find file at path: {fileName}");
+            }
+            var image = Image.FromFile(fileName);
+            int expectedLength = 8;
+            int actualLength = TextProcessor.ParseBalance(image).ToString().Length;
+
+            Assert.Equal(expectedLength, actualLength);
+        }
+
+        [Fact]
+        public void ParseBalanceFromImage_1253000()
+        {
+            string fileName = @".\samples\Tests\BalanceImageSamples\1253000.png";
+            if (!File.Exists(fileName))
+            {
+                throw new ArgumentException($"Could not find file at path: {fileName}");
+            }
+            var image = Image.FromFile(fileName);
+            int expectedLength = 7;
+            int actualLength = TextProcessor.ParseBalance(image).ToString().Length;
+
+            Assert.Equal(expectedLength, actualLength);
+        }
     }
 }
