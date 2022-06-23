70729babb0ca81b61db94fed05128af37828910e
filadog <filimonov1995@gmail.com>
2022-06-24 02:38:29 +0300

    Refactoring

---
+++ TinyClicker/src/Interfaces/ISampleImage.cs
@@ -0,0 +1,14 @@
+﻿using OpenCvSharp;
+using System;
+
+
+namespace TinyClicker.Interfaces;
+
+internal interface ISampleImage
+{
+    public int OrderedNumber { get; }
+    string ImageName { get; }
+    Mat Mat { get; }
+
+    Mat GenerateMat();
+}
