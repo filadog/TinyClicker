b433a4900d32d50e5f8e315768ae163abf752659
filadog <51026900+filadog@users.noreply.github.com>
2023-12-17 00:15:20 +0300

    simple viewmodel for user settings

---
+++ TinyClicker.UI/ViewModels/UserSettingsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TinyClicker.UI.ViewModels;
+
+public class UserSettingsViewModel
+{
+    public int CurrentFloor { get; set; }
+    public int RebuildAtFloor { get; set; }
+    public int WatchAdsFromFloor { get; set; }
+    public bool WatchBuxAds { get; set; }
+    public DateTime LastRebuildTime { get; set; }
+    public bool BuildFloors { get; set; }
+    public DateTime LastRaffleTime { get; set; }
+    public int FloorCostDecreasePercent { get; set; }
+}
